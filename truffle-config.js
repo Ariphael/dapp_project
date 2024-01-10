aae466e4a8e2f6174915d98291d1ec4242ad7fa0
Richard Tochtermann <richard.tochtermann@outlook.com>
2024-01-10 13:56:36 +0100

    Added configuration parameters corresponding to local network in truffle-config.js. Network configuration parameters including port and network_id was copied over from the DApp development tutorial

--- truffle-config.js
+++ truffle-config.js
@@ -58,6 +58,11 @@ module.exports = {
    */
 
   networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "5777"
+    }
     // Useful for testing. The `development` name is special - truffle uses it by default
     // if it's defined here and no other network is specified at the command line.
     // You should run a client (like ganache, geth, or parity) in a separate terminal
