deee6dec12af26e6ade601b20ea395eb283fdddc
Krzysztof Madajczak <krzysztof.madajczakbiznes@gmail.com>
2024-01-17 15:47:41 +0100

    2-SC-add-more-logic

--- contracts/secondSC.sol
+++ contracts/secondSC.sol
@@ -1,39 +1,68 @@
 pragma solidity ^0.5.2;
+pragma experimental ABIEncoderV2;
 
-contract secondSC  {
+contract secondSC  
+{
+  bool private votingPhaseFlag;
   address private electionContractAddress;
   mapping(address => bool) hasVoted;
+  mapping(address => bool) isCandidate; 
   mapping(address => int) numberofVotes;
-  bool private votingPhaseFlag;
+  Candidate[] public candidates;
 
-
-  modifier onlyElectionContractCanCall() {
-    require(msg.sender == electionContractAddress);
-    _;
+  struct Candidate 
+  {
+    int id;
+    string firstName;
+    string lastName;
+    address candidateAddress;
   }
 
-  constructor(address[] memory candidates) public {
+  
+  constructor(Candidate[] memory _candidates) public 
+  {
     electionContractAddress = msg.sender;
     votingPhaseFlag = true;
 
-    for (uint i = 0; i < candidates.length; i++) {
-      isCandidate[candidates[i]] = true;
+    for (uint i = 0; i < _candidates.length; i++) 
+    {
+        candidates.push(Candidate({
+            id: int(i),
+            firstName: _candidates[i].firstName,
+            lastName: _candidates[i].lastName,
+            candidateAddress: _candidates[i].candidateAddress
+        }));
+        isCandidate[_candidates[i].candidateAddress] = true;
     }
   }
 
   function vote(address candidate) external
   {
-    require(VotingPhaseFlag == true, "Operation denied. Election is not in voting phase.");
-    require(!hasVoted[msg.sender], "Operation denied. You are already Voted in this election.");
+    require(votingPhaseFlag == true, "Operation denied. Election is not in voting phase.");
+    require(!hasVoted[msg.sender], "Operation denied. You are already voted in this election.");
     hasVoted[msg.sender] = true;
-    // Implement anti race condition
-    numberOfVotes[candidate] += 1;
+    require(isCandidate[candidate], "Operation denied. Candidate is not in the list.");
+    // Implement anti-race condition
+    numberofVotes[candidate] += 1;
+  }
+  
+  function getVotes(address candidate) public view returns (int) 
+  {
+    return numberofVotes[candidate];
+  }
 
-  }
-  function getVotes(address candidate) public view returns (int) {
-    return numberOfVotes[candidate];
-  }
-  function endRegistrationPhase() public onlyElectionContractCanCall {
-    votingPhaseFlag = false;
+  function getCandidate(address candidate) public view returns (int, string memory, string memory, address) 
+  {
+    for (uint i = 0; i < candidates.length; i++) {
+        if (candidates[i].candidateAddress == candidate) {
+            return (candidates[i].id, candidates[i].firstName, candidates[i].lastName, candidates[i].candidateAddress);
+        }
+    }
+    revert("Candidate not found");
   }
 }
+
+
+  // function endVotingPhase() public onlyElectionContractCanCall {
+  //   votingPhaseFlag = false;
+  // }
